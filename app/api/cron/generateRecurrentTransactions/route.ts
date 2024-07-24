97c1f751e1f234d98fade2c28af6a03736cf962c
Xavier Garcia <xavier.garcia@prometeo.dev>
2024-07-23 20:31:05 -0500

    Adding logs in cronjob

--- app/api/cron/generateRecurrentTransactions/route.ts
+++ app/api/cron/generateRecurrentTransactions/route.ts
@@ -4,13 +4,17 @@ import { getStatusError } from "@/src/utils/errors/get-status-status"
 import type { NextRequest } from "next/server"
 
 export async function GET(request: NextRequest) {
+  console.log("Cron job started: generateRecurrentTransactions")
+
   const authHeader = request.headers.get("authorization")
   if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    console.log("Unauthorized to execute this cronjob")
     return new Response("Unauthorized", {
       status: 401,
     })
   }
 
+  console.log("Generating recurrent transactions")
   const transactions = []
   try {
     const newTransactions = await generateRecurrentTransactions()
