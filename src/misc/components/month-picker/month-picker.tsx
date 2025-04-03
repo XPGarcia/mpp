7385450f91dd0965ead917d3169d0a4c2e4c8167
Xavier Garcia <xavier.garcia@prometeo.com>
2025-04-02 22:34:01 -0500

    Chore: Remove max date from month picker

--- src/misc/components/month-picker/month-picker.tsx
+++ src/misc/components/month-picker/month-picker.tsx
@@ -30,7 +30,6 @@ export const MonthPicker = ({ defaultValue, onChange }: Props) => {
     <input
       type='month'
       value={yearMonth}
-      max={dayjs(new Date()).format("YYYY-MM")}
       className='block h-[30px] w-fit rounded-md border border-shades-50 bg-white px-2 text-xs text-gray-700 placeholder-gray-400/70 hover:border-shades-400 focus:border-blue-400 focus:outline-none focus:outline-1 focus:-outline-offset-2 focus:outline-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-40 sm:px-5'
       onChange={handleChangeMonth}
     />
